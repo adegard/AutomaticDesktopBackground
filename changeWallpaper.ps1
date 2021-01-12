9fb87daa9ec1510e79310398ded7afbbccff844c
adegard <degardinarnaud@gmail.com>
2021-01-12 09:04:42 +0100

    Update changeWallpaper.ps1

--- changeWallpaper.ps1
+++ changeWallpaper.ps1
@@ -1,3 +1,4 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-windowstyle hidden -NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
-iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/adegard/AutomaticDesktopBackground/main/NewDesktopImage.ps1'))
+$keyword="fantasy"
+iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/adegard/AutomaticDesktopBackground/main/PersonalizedWallpaper.ps1'))
